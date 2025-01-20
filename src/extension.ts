21538d7d7ca4651fc5f7a6e58d145e73bdaa29ad
dankinsoid <30962149+dankinsoid@users.noreply.github.com>
2025-01-21 01:21:58 +0300

    refactor: Remove duplicate import and clean up extension activation comment

--- src/extension.ts
+++ src/extension.ts
@@ -2,10 +2,6 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-import * as vscode from 'vscode';
-
 interface BracketPair {
     open: string;
     close: string;
